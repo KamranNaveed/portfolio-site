e3dfdcae6cd4bbe16e4cb4165781a9b3cf89de03
KamranNaveed <kamran.naveedsyed@gmail.com>
2023-01-06 20:29:41 -0500

    responsive design

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector(".hamburger")
 const navOptions = document.querySelector(".nav-options")
 
+
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active")
     navOptions.classList.toggle("active")
 })
+
+document.querySelectorAll("nav-option--items").forEach(item=>item.addEventListener("click", ()=>{
+    hamburger.classList.remove("active")
+    navOptions.classList.remove("active")
+
+}))
